745d3256c458b842046a9229474e23ee8b4d64f7
Andreas Prang <A.Prang@TenStepsAhead.de>
2015-06-09 12:19:22 +0200

    created fortuneController

--- StackOfFortune/StackOfFortune/ViewController.m
+++ StackOfFortune/StackOfFortune/ViewController.m
@@ -19,6 +19,10 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)viewDidAppear:(BOOL)animated
+{
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
