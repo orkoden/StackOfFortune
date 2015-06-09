655f01d8e28401d10075d6e3e6a414beced06abe
Jörg Bühmann <joergbuehmann@gmail.com>
2015-06-09 16:53:07 +0200

    Adds complete spotlight indexing. broken

--- StackOfFortune/StackOfFortune/FortuneController.m
+++ StackOfFortune/StackOfFortune/FortuneController.m
@@ -7,6 +7,8 @@
 //
 
 #import "FortuneController.h"
+@import CoreSpotlight;
+@import MobileCoreServices;
 
 @implementation FortuneController
 
@@ -27,10 +29,43 @@
 			Wisdom *wisdom = [Wisdom wisdomWithQuote:wisdomSubstrings.firstObject andAuthor:wisdomSubstrings.lastObject];
 			[mutableWisdoms addObject:wisdom];
 		}
-		
+	
+        [self addSpotlightSearchForWisdoms:mutableWisdoms];
+
+        
 		self.wisdoms = (NSArray *)mutableWisdoms;
 	}
 	return self;
+}
+
+-(void) addSpotlightSearchForWisdoms: (NSArray*) wisdoms
+{
+    NSMutableArray* indexWisdoms = [NSMutableArray arrayWithCapacity:wisdoms.count];
+    for (Wisdom* wisdom in wisdoms) {
+
+        // Create an attribute set for an item that represents an image.
+        CSSearchableItemAttributeSet* attributeSet = [[CSSearchableItemAttributeSet alloc] initWithItemContentType:(NSString*) kUTTypeText];
+        // Set properties that describe attributes of the item such as title, description, and image.
+        attributeSet.title = wisdom.author;
+        attributeSet.displayName = wisdom.author;
+        if (wisdom.author && wisdom.quote) {
+            attributeSet.keywords = @[wisdom.author, wisdom.quote];
+        }
+        attributeSet.contentDescription = wisdom.quote;
+        //        Next, create a CSSearchableItem object to represent the item and add it to the index. Listing 4 shows how to create a CSSearchableItem object and index it.
+        
+        // Create a searchable item, specifying its ID, associated domain, and attribute set.
+        CSSearchableItem* item;
+        item = [[CSSearchableItem alloc] initWithUniqueIdentifier:nil domainIdentifier:@"bla.laber.com" attributeSet:attributeSet];
+        // Index the item.
+        [indexWisdoms addObject:item];
+    }
+    
+    
+    [[CSSearchableIndex defaultSearchableIndex] indexSearchableItems: indexWisdoms completionHandler: ^(NSError * __nullable error) {
+        NSLog(@"Search item indexed");
+    }];
+    
 }
 
 @end
