abe892acbdcda3cb9114a906f8259c865612e1cf
Andreas Prang <A.Prang@TenStepsAhead.de>
2015-06-09 12:20:55 +0200

    created FortuneController

---
+++ StackOfFortune/StackOfFortune/FortuneController.h
@@ -0,0 +1,13 @@
+//
+//  FortuneController.h
+//  StackOfFortune
+//
+//  Created by Andreas Prang on 09.06.15.
+//  Copyright © 2015 BWDC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FortuneController : NSObject
+
+@end
---
+++ StackOfFortune/StackOfFortune/FortuneController.m
@@ -0,0 +1,13 @@
+//
+//  FortuneController.m
+//  StackOfFortune
+//
+//  Created by Andreas Prang on 09.06.15.
+//  Copyright © 2015 BWDC. All rights reserved.
+//
+
+#import "FortuneController.h"
+
+@implementation FortuneController
+
+@end
